b034bf281f598e1168d7351aeb38526bea38f37f
Grigoriy Kosterev <kosterev@yandex.ru>
2019-04-13 23:15:04 +0300

    add validation

--- api.py
+++ api.py
@@ -9,9 +9,9 @@ import uuid
 from optparse import OptionParser
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-SALT = "Otus"
-ADMIN_LOGIN = "admin"
-ADMIN_SALT = "42"
+SALT = 'Otus'
+ADMIN_LOGIN = 'admin'
+ADMIN_SALT = '42'
 OK = 200
 BAD_REQUEST = 400
 FORBIDDEN = 403
@@ -19,44 +19,93 @@ NOT_FOUND = 404
 INVALID_REQUEST = 422
 INTERNAL_ERROR = 500
 ERRORS = {
-    BAD_REQUEST: "Bad Request",
-    FORBIDDEN: "Forbidden",
-    NOT_FOUND: "Not Found",
-    INVALID_REQUEST: "Invalid Request",
-    INTERNAL_ERROR: "Internal Server Error",
+    BAD_REQUEST: 'Bad Request',
+    FORBIDDEN: 'Forbidden',
+    NOT_FOUND: 'Not Found',
+    INVALID_REQUEST: 'Invalid Request',
+    INTERNAL_ERROR: 'Internal Server Error',
 }
 UNKNOWN = 0
 MALE = 1
 FEMALE = 2
 GENDERS = {
-    UNKNOWN: "unknown",
-    MALE: "male",
-    FEMALE: "female",
+    UNKNOWN: 'unknown',
+    MALE: 'male',
+    FEMALE: 'female',
 }
+
 
 class Field:
 
-    def __init__(self, required, nullable):
-        self.required = required
-        self.nullable = nullable
+    def __init__(self, required, nullable, key, value):
+        self._required = required
+        self._nullable = nullable
+        self._key = key
+        self._value = value
+
+
+    @property
+    def required(self):
+        return self._required
+
+    @property
+    def nullable(self):
+        return self._nullable
+
+    # def validation(self):
+    #     if self._required:
+    #         if self._nullable:
+    #             return True
+    #         else:
+    #             if self.value:
+    #                 return True
+    #             else:
+    #                 return False
+    #     else:
+    #         return True
+
+    def validation(self):
+        raise NotImplementedError
+
+    @property
+    def key(self):
+        return self._key
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def set_value(self, value):
+        self._value = value
+
 
 
 class CharField(Field):
 
-    def __init__(self, required, nullable):
-        super().__init__(self, required, nullable)
+    def __init__(self, required, nullable, key, value):
+        super().__init__(self, required, nullable, key, value)
+
+    def validation(self):
+        return isinstance(self.value, str)
+
 
 
 class ArgumentsField(Field):
 
-    def __init__(self, required, nullable):
-        super().__init__(self, required, nullable)
+    def __init__(self, required, nullable, key, value):
+        super().__init__(self, required, nullable, key, value)
+
 
 
 class EmailField(CharField):
 
-    def __init__(self, required, nullable):
-        super().__init__(self, required, nullable)
+    def __init__(self, required, nullable, key, value):
+        super().__init__(self, required, nullable, 'email', value)
+
+    def validation(self):
+        return super().validation() and '@' in self.value
+
 
 
 class PhoneField(Field):
@@ -64,6 +113,11 @@ class PhoneField(Field):
     def __init__(self, required, nullable):
         super().__init__(self, required, nullable)
 
+    def validation(self):
+        if isinstance(self.value, str) or isinstance(self.value, int):
+            pass
+        else:
+            return False
 
 class DateField(Field):
 
@@ -71,16 +125,29 @@ class DateField(Field):
         super().__init__(self, required, nullable)
 
 
-class BirthDayField(Field):
+class BirthDayField(CharField):
 
-    def __init__(self, required, nullable):
-        super().__init__(self, required, nullable)
+    def __init__(self, required, nullable, key, value):
+        super().__init__(self, required, nullable, 'birthday', value)
+
+    def validation(self):
+        if super().validation() is False:
+            return False
+        try:
+            datetime.datetime.strptime(self.value, '%d.%m.%Y')
+        except ValueError:
+            logging.error('Incorrect data format, should be DD.MM.YYYY')
+            return False
+        return True
 
 
 class GenderField(Field):
 
     def __init__(self, required, nullable):
         super().__init__(self, required, nullable)
+
+    def validation(self):
+        return isinstance(self.value, int) and self.value in GENDERS
 
 
 class ClientIDsField(Field):
@@ -99,11 +166,31 @@ class MethodRequest:
         self.method = CharField(required=True, nullable=False)
 
     @property
-    def is_admin(self):
+    def _is_admin(self):
         return self.login == ADMIN_LOGIN
 
-    def method_handler(self):
+    def _check_auth(self):
+        if self._is_admin:
+            digest = hashlib.sha512(datetime.datetime.now().strftime('%Y%m%d%H') + ADMIN_SALT).hexdigest()
+        else:
+            digest = hashlib.sha512(self.account + self.login + SALT).hexdigest()
+        if digest == self.token:
+            return True
+        return False
+
+    def _validation(self):
         pass
+
+    def handler(self):
+        self._check_auth()
+        self._validation()
+        self.method_handler()
+
+    def validation(self):
+        raise NotImplementedError
+
+    def post(self):
+        raise NotImplementedError
 
 
 class ClientsInterestsRequest(MethodRequest):
@@ -113,14 +200,17 @@ class ClientsInterestsRequest(MethodRequest):
         self.client_ids = ClientIDsField(required=True)
         self.date = DateField(required=False, nullable=True)
 
-    def method_handler(self):
-        pass
+    def method_handler(self, request, ctx, store):
+        if self.check_auth():
+            pass
 
+    def validation(self):
+        pass
 
 class OnlineScoreRequest(MethodRequest):
 
     def __init__(self):
-        super().__init__(self)
+        super().__init__()
         self.first_name = CharField(required=False, nullable=True)
         self.last_name = CharField(required=False, nullable=True)
         self.email = EmailField(required=False, nullable=True)
@@ -128,12 +218,13 @@ class OnlineScoreRequest(MethodRequest):
         self.birthday = BirthDayField(required=False, nullable=True)
         self.gender = GenderField(required=False, nullable=True)
 
-    def method_handler(self):
+    def method_handler(self, request, ctx, store):
         pass
+
 
 def check_auth(request):
     if request.is_admin:
-        digest = hashlib.sha512(datetime.datetime.now().strftime("%Y%m%d%H") + ADMIN_SALT).hexdigest()
+        digest = hashlib.sha512(datetime.datetime.now().strftime('%Y%m%d%H') + ADMIN_SALT).hexdigest()
     else:
         digest = hashlib.sha512(request.account + request.login + SALT).hexdigest()
     if digest == request.token:
@@ -152,12 +243,16 @@ class MainHTTPHandler(BaseHTTPRequestHandler):
     }
     store = None
 
-    def get_request_id(self, headers):
+    @staticmethod
+    def get_request_id(headers):
         return headers.get('HTTP_X_REQUEST_ID', uuid.uuid4().hex)
 
-    def do_POST(self):
+    def get_field_list(self, request):
+        return []
+
+    def do_post(self):
         response, code = {}, OK
-        context = {"request_id": self.get_request_id(self.headers)}
+        context = {'request_id': self.get_request_id(self.headers)}
         request = None
         try:
             data_string = self.rfile.read(int(self.headers['Content-Length']))
@@ -166,39 +261,40 @@ class MainHTTPHandler(BaseHTTPRequestHandler):
             code = BAD_REQUEST
 
         if request:
-            path = self.path.strip("/")
-            logging.info("%s: %s %s" % (self.path, data_string, context["request_id"]))
+            context['has'] = self.get_field_list(request)
+            path = self.path.strip('/')
+            logging.info('%s: %s %s' % (self.path, data_string, context['request_id']))
             if path in self.router:
                 try:
-                    response, code = self.router[path]({"body": request, "headers": self.headers}, context, self.store)
+                    response, code = self.router[path]({'body': request, 'headers': self.headers}, context, self.store)
                 except Exception as e:
-                    logging.exception("Unexpected error: %s" % e)
+                    logging.exception('Unexpected error: %s' % e)
                     code = INTERNAL_ERROR
             else:
                 code = NOT_FOUND
 
         self.send_response(code)
-        self.send_header("Content-Type", "application/json")
+        self.send_header('Content-Type', 'application/json')
         self.end_headers()
         if code not in ERRORS:
-            r = {"response": response, "code": code}
+            r = {'response': response, 'code': code}
         else:
-            r = {"error": response or ERRORS.get(code, "Unknown Error"), "code": code}
+            r = {'error': response or ERRORS.get(code, 'Unknown Error'), 'code': code}
         context.update(r)
         logging.info(context)
         self.wfile.write(json.dumps(r))
         return
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     op = OptionParser()
-    op.add_option("-p", "--port", action="store", type=int, default=8080)
-    op.add_option("-l", "--log", action="store", default=None)
+    op.add_option('-p', '--port', action='store', type=int, default=8080)
+    op.add_option('-l', '--log', action='store', default=None)
     (opts, args) = op.parse_args()
     logging.basicConfig(filename=opts.log, level=logging.INFO,
                         format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
-    server = HTTPServer(("localhost", opts.port), MainHTTPHandler)
-    logging.info("Starting server at %s" % opts.port)
+    server = HTTPServer(('localhost', opts.port), MainHTTPHandler)
+    logging.info('Starting server at %s' % opts.port)
     try:
         server.serve_forever()
     except KeyboardInterrupt:
